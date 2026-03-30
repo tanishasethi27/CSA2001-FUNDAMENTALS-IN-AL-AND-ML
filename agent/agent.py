f4c1d7c8a3f0d0bd5ef5cf872238db77e79baed8
Tanisha Sethi <tanisha.25bai11491@vitbhopal.ac.in>
2026-03-30 23:19:05 +0530

    Create agent.py

---
+++ agent/agent.py
@@ -0,0 +1,10 @@
+class Agent:
+    def __init__(self, start, goal):
+        self.start = start
+        self.goal = goal
+
+    def get_start(self):
+        return self.start
+
+    def get_goal(self):
+        return self.goal
